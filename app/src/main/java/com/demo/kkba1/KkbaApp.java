2c1fc6ab4ddc49209856cb5e8865f4558626cb13
Kris <s>
2022-10-04 22:30:36 +0800

    3.3.1-Learning-Task-3.1

---
+++ app/src/main/java/com/demo/kkba1/KkbaApp.java
@@ -0,0 +1,13 @@
+package com.demo.kkba1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class KkbaApp extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
